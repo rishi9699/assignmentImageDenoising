1f3b302aeaec8b884ee2a640f3208d83ecf0dda9
Shashank Shantaram Padwal <37622033+shashank23p@users.noreply.github.com>
2021-02-04 23:52:01 +0530

    tc

--- raw_code.m
+++ raw_code.m
@@ -2,18 +2,19 @@ curimg = imnoi;
 alpha=0.5;
 
 for passes = 1:10
-for i=2:255
-    for j=2:255
-        xi = curimg(i,j);
+    disp("pass number"+passes);
+    for i=2:255
+        for j=2:255
+            xi = curimg(i,j);
 
 
-        %minimizing for each pixel..
-        %quadratic prior and should use circshift
-        %((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)
-        objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * ((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)) ;
-        curimg(i,j) = fminbnd(objective_pixel,0,1);
-        
+            %minimizing for each pixel..
+            %quadratic prior and should use circshift
+            %((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)
+            objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * ((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)) ;
+            curimg(i,j) = fminbnd(objective_pixel,0,1);
+
+        end
     end
-end
 end
 disp(myfun())
