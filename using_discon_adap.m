ce7899d3fbb61f5dd5b806642d96a879a97e4609
rishi9699 <rishi9699@gmail.com>
2021-02-05 14:05:23 +0530

    major error correction in discontinuity adaptive expression written gradient descent loop with fixed step size

--- using_discon_adap.m
+++ using_discon_adap.m
@@ -16,10 +16,16 @@ for passes = 1:10
             %minimizing for each pixel..
             %discon-adapt prior and should use circshift
             
-            objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * gamma * (abs(top-xi) + abs(bottom-xi) +abs(left-xi) + abs(right-xi)) - gamma^2 * log( (1+abs(top-xi)/gamma) * (1+abs(bottom-xi)/gamma) * (1+abs(left-xi)/gamma) * (1+abs(right-xi)/gamma) ));
+            objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * (gamma * (abs(top-xi) + abs(bottom-xi) +abs(left-xi) + abs(right-xi)) - gamma^2 * log( (1+abs(top-xi)/gamma) * (1+abs(bottom-xi)/gamma) * (1+abs(left-xi)/gamma) * (1+abs(right-xi)/gamma) )));
             curimg(i,j) = fminbnd(objective_pixel,0,1);
             
 
         end
     end
+end
+
+st=4;
+for iter=1:5000
+xi = xi - st * (alpha*2*(xi-imnoi(i,j)) + (1-alpha)*gamma*( (xi-top)/(gamma+abs(xi-top)) + (xi-bottom)/(gamma+abs(xi-bottom)) + (xi-left)/(gamma+abs(xi-left)) + (xi-right)/(gamma+abs(xi-right)) ));
+%disp(xi)
 end
