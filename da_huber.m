f426331407d07896ed2e390701a58fabdd4b56be
rishi9699 <rishi9699@gmail.com>
2021-02-05 15:27:03 +0530

    Many changes for implementing the gradient descent

--- da_huber.m
+++ da_huber.m
@@ -6,16 +6,54 @@ for passes = 1:3
     for i=2:255
         for j=2:255
             xi = curimg(i,j);
+            top = curimg(i-1,j);
+            bottom = curimg(i+1,j);
+            left = curimg(i,j-1);
+            right = curimg(i,j+1);
+            y=imnoi(i,j);
 
 
             %minimizing for each pixel..
             %quadratic prior and should use circshift
             %disp("current posterior "+ (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * huberpriorsum(xi,curimg(i-1,j),curimg(i+1,j),curimg(i,j-1),curimg(i,j+1))));
-            objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * huberpriorsum(xi,curimg(i-1,j),curimg(i+1,j),curimg(i,j-1),curimg(i,j+1))) ;
+            objective_pixel = @(xi) (alpha * (abs(y - xi))^2 + (1-alpha) * huberpriorsum(xi,top,bottom,left,right)) ;
             curimg(i,j) = fminbnd(objective_pixel,0,1);
+            
+            %Using gradient descent
+            
+            for iter=1:1000
+                gradient_sum=0;
+                if abs(xi-top)<=gamma
+                    gradient_sum = gradient_sum+xi-top;
+                else
+                    gradient_sum = gradient_sum+gamma*sign(xi-top);
+                end
+
+                if abs(xi-bottom)<=gamma
+                    gradient_sum = gradient_sum+xi-bottom;
+                else
+                    gradient_sum = gradient_sum+gamma*sign(xi-bottom);
+                end
+
+                if abs(xi-left)<=gamma
+                    gradient_sum = gradient_sum+xi-left;
+                else
+                    gradient_sum = gradient_sum+gamma*sign(xi-left);
+                end
+
+                if abs(xi-right)<=gamma
+                    gradient_sum = gradient_sum+xi-right;
+                else
+                    gradient_sum = gradient_sum+gamma*sign(xi-right);
+                end
+
+                xi = xi - st * (alpha*2*(xi-y) + (1-alpha)*gradient_sum);
+            end
+            disp(xi)
             %disp("optimizied posterior "+ (alpha * (abs(imnoi(i,j) - curimg(i,j)))^2 + (1-alpha) * huberpriorsum(curimg(i,j),curimg(i-1,j),curimg(i+1,j),curimg(i,j-1),curimg(i,j+1))));
 
         end
     end
 end
-disp(myfun())
+
+
--- raw_code.m
+++ raw_code.m
@@ -1,21 +1,35 @@
 curimg = imnoi;
 alpha=0.5;
 
-for passes = 1:3
+for passes = 1:5
     disp("pass number"+passes);
     for i=2:255
         for j=2:255
             xi = curimg(i,j);
+            top = curimg(i-1,j);
+            bottom = curimg(i+1,j);
+            left = curimg(i,j-1);
+            right = curimg(i,j+1);
+            y=imnoi(i,j);
 
 
             %minimizing for each pixel..
             %quadratic prior and should use circshift
             %((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)
             %disp("current posterior "+ (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * ((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)));
+            
             objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * ((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)) ;
             curimg(i,j) = fminbnd(objective_pixel,0,1);
+            
             %disp("optimizied posterior "+ (alpha * (abs(imnoi(i,j) - curimg(i,j)))^2 + (1-alpha) * ((curimg(i,j)-curimg(i-1,j))^2 + (curimg(i,j)-curimg(i+1,j))^2 + (curimg(i,j)-curimg(i,j-1))^2 + (curimg(i,j)-curimg(i,j+1))^2)));
-
+            
+            %use very small step size
+            %st=0.01
+            %for iter=1:400
+            %xi = xi - st * (alpha*2*(xi-imnoi(i,j)) + (1-alpha)*2*(4*xi -top-bottom-left-right) );
+            %disp(xi)
+            %end
+            %curimg(i,j) = xi;
         end
     end
 end
