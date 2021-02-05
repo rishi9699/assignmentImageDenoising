597815f33e11c09a9da5a674e19f924b4fe793d2
rishi9699 <rishi9699@gmail.com>
2021-02-05 16:11:53 +0530

    Adaptive step size for gaussian MRF

--- raw_code.m
+++ raw_code.m
@@ -18,19 +18,33 @@ for passes = 1:5
             %((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)
             %disp("current posterior "+ (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * ((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)));
             
-            objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * ((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)) ;
-            curimg(i,j) = fminbnd(objective_pixel,0,1);
+            %%objective_pixel = @(xi) (alpha * (abs(imnoi(i,j) - xi))^2 + (1-alpha) * ((xi-curimg(i-1,j))^2 + (xi-curimg(i+1,j))^2 + (xi-curimg(i,j-1))^2 + (xi-curimg(i,j+1))^2)) ;
+            %%curimg(i,j) = fminbnd(objective_pixel,0,1);
             
             %disp("optimizied posterior "+ (alpha * (abs(imnoi(i,j) - curimg(i,j)))^2 + (1-alpha) * ((curimg(i,j)-curimg(i-1,j))^2 + (curimg(i,j)-curimg(i+1,j))^2 + (curimg(i,j)-curimg(i,j-1))^2 + (curimg(i,j)-curimg(i,j+1))^2)));
             
             %use very small step size
             %st=0.01
-            %for iter=1:400
-            %xi = xi - st * (alpha*2*(xi-imnoi(i,j)) + (1-alpha)*2*(4*xi -top-bottom-left-right) );
-            %disp(xi)
-            %end
+%             for iter=1:400
+%             xi = xi - st * (alpha*2*(xi-imnoi(i,j)) + (1-alpha)*2*(4*xi -top-bottom-left-right) );
+%             disp(xi)
+%             end
             %curimg(i,j) = xi;
+            
+            st=0.01;
+            while st>=0.001                
+                xi_new = xi - st * (alpha*2*(xi-y) + (1-alpha)*2*(4*xi -top-bottom-left-right) );
+                new_obj = alpha * (abs(y - xi_new))^2 + (1-alpha) * ((xi_new-top)^2 + (xi_new-bottom)^2 + (xi_new-left)^2 + (xi_new-right)^2);
+                old_obj = alpha * (abs(y - xi))^2 + (1-alpha) * ((xi-top)^2 + (xi-bottom)^2 + (xi-left)^2 + (xi-right)^2);
+                if new_obj<old_obj
+                    xi = xi_new;
+                    st=1.1*st;
+                else
+                    st=st*0.5;
+                end
+            end
+            curimg(i,j) = xi;
+            
         end
     end
 end
-disp(myfun())
