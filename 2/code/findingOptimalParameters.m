0d1a1a51d2bfba5d981ba2eeae74a19b94b92b4e
rishi9699 <rishi9699@gmail.com>
2021-02-06 22:42:42 +0530

    many more changes

--- 2/code/findingOptimalParameters.m
+++ 2/code/findingOptimalParameters.m
@@ -7,7 +7,7 @@ for alpha=0.1:0.1:1
     rrmse(counter) = sqrt(sum((brainMRIsliceOrig - quadraticDenoisedImage).^2))/sqrt(sum(brainMRIsliceOrig.^2));
     counter=counter+1;
 end
-% alpha=0.9 gives lowest RRMSE=0.2154
+% alpha=0.9 gives lowest RRMSE=0.1078
 % Further refining
 
 rrmse=zeros(1,21);
@@ -17,29 +17,29 @@ for alpha=0.8:0.01:1
     rrmse(counter) = sqrt(sum((brainMRIsliceOrig - quadraticDenoisedImage).^2))/sqrt(sum(brainMRIsliceOrig.^2));
     counter=counter+1;
 end
-% alpha=0.92 and 0.93 gives lowest RRMSE=0.2151
-% Choosing alpha_quadratic=0.92 as optimal
+% alpha=0.87 gives lowest RRMSE=0.1071
+% Choosing alpha_quadratic=0.88 as optimal
 
 %Finding the optimal parameters for Huber MRF
 rrmse=zeros(1,70);
 counter=1;
-for alpha=0.01:0.01:0.07
+for alpha=0.10:0.01:0.20
     for gamma=0.001:0.001:0.01
         [huberDenoisedImage, ~] = denoiseHuberMRF(alpha, gamma, brainMRIsliceNoisy);
         rrmse(counter) = sqrt(sum((brainMRIsliceOrig - huberDenoisedImage).^2))/sqrt(sum(brainMRIsliceOrig.^2));
         counter=counter+1;
     end
 end
-%On further refining and trial-error, we get alpha_huber=0.008 and gamma_huber=0.001
+%On further refining and trial-error, we get alpha_huber=0.16 and gamma_huber=0.008
 
 %Finding the optimal parameters for discontinuity adaptive MRF
-rrmse=zeros(1,110);
+rrmse=zeros(1,121);
 counter=1;
-for alpha=0.030:0.001:0.040
-    for gamma=0.001:0.001:0.01
+for alpha=0.20:0.001:0.30
+    for gamma=0.010:0.001:0.02
         [da_DenoisedImage, ~] = denoiseDAdapMRF(alpha, gamma, brainMRIsliceNoisy);
         rrmse(counter) = sqrt(sum((brainMRIsliceOrig - da_DenoisedImage).^2))/sqrt(sum(brainMRIsliceOrig.^2));
         counter=counter+1;
     end
 end
-%On further refining and trial-error, we get alpha_da=0.033 and gamma_da=0.003
+%On further refining and trial-error, we get alpha_da=0.25 and gamma_da=0.018
